3e7fa1d37a8fbbbe325f0bcd0886e8b52b3b0dfe
XuJun <nankaimathxujun@gmail.com>
2017-03-30 17:12:04 +0800

    add

--- Demo_MLP_NoGT.m
+++ Demo_MLP_NoGT.m
@@ -8,16 +8,17 @@ setname          = 'Real_NoisyImage';
 method           =  'MLP';
 ref_folder       =  fullfile('C:\Users\csjunxu\Desktop\CVPR2017\1_Results\',setname);
 
-den_folder       =  ['Results_',setname,'_',method];
-if ~isdir(den_folder)
-    mkdir(den_folder)
+% write image directory
+write_sRGB_dir = ['C:/Users/csjunxu/Desktop/CVPR2017/1_Results/'];
+if ~isdir(write_sRGB_dir)
+    mkdir(write_sRGB_dir)
 end
 
 noise_levels     =  [10];
 images           =  dir(fullfile(ref_folder,'*.png'));
 format compact;
 
-for i = 1 : numel(images)
+for i = [5 7 8 14] %1 : numel(images)
     [~, name, exte]  =  fileparts(images(i).name);
     IM =   double(imread( fullfile(ref_folder,images(i).name) ));
     [h,w,ch] = size(IM);
@@ -37,7 +38,7 @@ for i = 1 : numel(images)
             IMoutcc = fdenoiseNeural(IM(:,:,c), nSig, model);
             IMout(:,:,c) = IMoutcc;
         end
-        imwrite(IMout/255, ['C:/Users/csjunxu/Desktop/ICCV2017/1nc_Results/Real_' method '/' method '_Real_' num2str(noise_levels) '_' name '.png']);
+        imwrite(IMout/255, [write_sRGB_dir 'Real_' method '/' method '_Real_' num2str(noise_levels) '_' name '.png']);
     end
 end
 
