d522e97b5423e1136fcf26aa2dda2288c9960ecb
XuJun <nankaimathxujun@gmail.com>
2017-09-25 22:56:36 +0800

    add

--- Demo_MLP_GT.m
+++ Demo_MLP_GT.m
@@ -7,23 +7,28 @@ clear;
 % GT_Original_image_dir = 'C:\Users\csjunxu\Desktop\CVPR2017\cc_Results\Real_MeanImage\';
 % GT_fpath = fullfile(GT_Original_image_dir, '*.png');
 % TT_Original_image_dir = 'C:\Users\csjunxu\Desktop\CVPR2017\cc_Results\Real_NoisyImage\';
-% TT_fpath = fullfile(TT_Original_image_dir, '*.png');
+% TT_fpath = fullfile(TT_Original_image_dir, '*.png'); 
 % GT_Original_image_dir = 'C:\Users\csjunxu\Desktop\CVPR2017\cc_Results\Real_ccnoise_denoised_part\';
 % GT_fpath = fullfile(GT_Original_image_dir, '*mean.png');
 % TT_Original_image_dir = 'C:\Users\csjunxu\Desktop\CVPR2017\cc_Results\Real_ccnoise_denoised_part\';
 % TT_fpath = fullfile(TT_Original_image_dir, '*real.png');
-GT_Original_image_dir = 'C:\Users\csjunxu\Desktop\CVPR2017\our_Results\Real_MeanImage\';
-GT_fpath = fullfile(GT_Original_image_dir, '*.JPG');
-TT_Original_image_dir = 'C:\Users\csjunxu\Desktop\CVPR2017\our_Results\Real_NoisyImage\';
-TT_fpath = fullfile(TT_Original_image_dir, '*.JPG');
+% GT_Original_image_dir = 'C:\Users\csjunxu\Desktop\CVPR2017\our_Results\Real_MeanImage\';
+% GT_fpath = fullfile(GT_Original_image_dir, '*.JPG');
+% TT_Original_image_dir = 'C:\Users\csjunxu\Desktop\CVPR2017\our_Results\Real_NoisyImage\';
+% TT_fpath = fullfile(TT_Original_image_dir, '*.JPG');
+GT_Original_image_dir = 'C:/Users/csjunxu/Desktop/RID_Dataset/RealisticImage/';
+GT_fpath = fullfile(GT_Original_image_dir, '*mean.JPG');
+TT_Original_image_dir = 'C:/Users/csjunxu/Desktop/RID_Dataset/RealisticImage/';
+TT_fpath = fullfile(TT_Original_image_dir, '*real.JPG');
 
 GT_im_dir  = dir(GT_fpath);
 TT_im_dir  = dir(TT_fpath);
 im_num = length(TT_im_dir);
 
-addpath 'model';
+addpath 'C:\Users\csjunxu\Desktop\JunXu\Paper\Image Video Denoising\MLP CVPR2012\MLP\model';
 method = 'MLP';
-write_sRGB_dir = ['C:/Users/csjunxu/Desktop/CVPR2017/our_Results/'];
+write_MAT_dir = ['C:/Users/csjunxu/Desktop/CVPR2018 Denoising/PolyU_Results/'];
+write_sRGB_dir = ['C:/Users/csjunxu/Desktop/CVPR2018 Denoising/PolyU_Results/' method];
 if ~isdir(write_sRGB_dir)
     mkdir(write_sRGB_dir)
 end
@@ -60,10 +65,10 @@ for nSig     =  [10]
         PSNR = [PSNR csnr( uint8(IMout), uint8(IM_GT), 0, 0 )];
         SSIM = [SSIM cal_ssim( uint8(IMout), uint8(IM_GT), 0, 0 )];
         fprintf('The final PSNR = %2.4f, SSIM = %2.4f. \n', PSNR(end), SSIM(end));
-        imwrite(IMout/255, [write_sRGB_dir 'Real_' method '/' method '_our' num2str(im_num) '_' IMname '.png']);
+        imwrite(IMout/255, [write_sRGB_dir '/' method '_our_' IMname '.png']);
     end
     mPSNR = mean(PSNR);
     mSSIM = mean(SSIM);
     mRunTime = mean(RunTime);
-    save([write_sRGB_dir method, '_our' num2str(im_num) '.mat'],'PSNR','mPSNR','SSIM','mSSIM','RunTime','mRunTime');
+    save([write_MAT_dir method, '_our.mat'],'PSNR','mPSNR','SSIM','mSSIM','RunTime','mRunTime');
 end
